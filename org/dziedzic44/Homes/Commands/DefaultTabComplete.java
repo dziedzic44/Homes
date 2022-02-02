4be8a16dd785e0487ea5d3495451d14b42d4dbc8
dziedzic44 <69324579+dziedzic44@users.noreply.github.com>
2022-02-02 18:20:29 +0100

    release

--- org/dziedzic44/Homes/Commands/DefaultTabComplete.java
+++ org/dziedzic44/Homes/Commands/DefaultTabComplete.java
@@ -7,7 +7,7 @@ import org.bukkit.command.TabCompleter;
 import org.bukkit.command.CommandSender;
 
 public class DefaultTabComplete implements TabCompleter {
-    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
         return new ArrayList<>();
     }
 }
