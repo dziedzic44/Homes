57bde0352b721e6bdd9f8eea43993ef4a0a399d8
dziedzic44 <69324579+dziedzic44@users.noreply.github.com>
2022-05-03 18:29:27 +0200

    update

--- org/dziedzic44/Homes/Main.java
+++ org/dziedzic44/Homes/Main.java
@@ -1,19 +1,19 @@
 package org.dziedzic44.Homes;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import org.dziedzic44.Homes.Commands.HomeCommand;
-import org.dziedzic44.Homes.Commands.DelhomeCommand;
-import org.dziedzic44.Homes.Commands.SethomeCommand;
 import org.dziedzic44.Homes.Commands.DefaultTabComplete;
+import org.dziedzic44.Homes.Commands.DelhomeCommand;
+import org.dziedzic44.Homes.Commands.HomeCommand;
+import org.dziedzic44.Homes.Commands.SethomeCommand;
 
 public class Main extends JavaPlugin {
     public void onEnable() {
         DataManager dataManager = new DataManager(this);
-        getCommand("home").setTabCompleter(new DefaultTabComplete());
         getCommand("delhome").setTabCompleter(new DefaultTabComplete());
+        getCommand("home").setTabCompleter(new DefaultTabComplete());
         getCommand("sethome").setTabCompleter(new DefaultTabComplete());
-        getCommand("home").setExecutor(new HomeCommand(this, dataManager));
         getCommand("delhome").setExecutor(new DelhomeCommand(this, dataManager));
+        getCommand("home").setExecutor(new HomeCommand(this, dataManager));
         getCommand("sethome").setExecutor(new SethomeCommand(this, dataManager));
         saveDefaultConfig();
     }
